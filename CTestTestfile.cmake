6c55a4c980fe2c60135e831154932236a0975428
YJ Choi <zappy.agi.choi@gmail.com>
2026-03-11 20:33:35 +0900

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/DSV
+# Build directory: /workspaces/DSV/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_consensus "/workspaces/DSV/build/test_consensus")
+set_tests_properties(test_consensus PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/DSV/CMakeLists.txt;158;add_test;/workspaces/DSV/CMakeLists.txt;0;")
+add_test(test_crypto "/workspaces/DSV/build/test_crypto")
+set_tests_properties(test_crypto PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/DSV/CMakeLists.txt;158;add_test;/workspaces/DSV/CMakeLists.txt;0;")
+add_test(test_serialize "/workspaces/DSV/build/test_serialize")
+set_tests_properties(test_serialize PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/DSV/CMakeLists.txt;158;add_test;/workspaces/DSV/CMakeLists.txt;0;")
+add_test(test_u320 "/workspaces/DSV/build/test_u320")
+set_tests_properties(test_u320 PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/DSV/CMakeLists.txt;158;add_test;/workspaces/DSV/CMakeLists.txt;0;")
